63467a35e75a92ea112dd892cc857063c8cfee34
Qurram Zaheer <qurram.zaheer@gmail.com>
2020-09-22 03:09:24 +0530

    Updates

---
+++ precache-manifest.0bfcadd400cdd4eb78c30182da56602b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9ea77c1346ccc7db94f9f60b1dd1e8",
+    "url": "/portfolio_v3/index.html"
+  },
+  {
+    "revision": "a3fd149e8c6d8afdcbd0",
+    "url": "/portfolio_v3/static/css/main.ee4494cd.chunk.css"
+  },
+  {
+    "revision": "083b30c4acfb3a9b852b",
+    "url": "/portfolio_v3/static/js/2.e6d07418.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/portfolio_v3/static/js/2.e6d07418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3fd149e8c6d8afdcbd0",
+    "url": "/portfolio_v3/static/js/main.78615625.chunk.js"
+  },
+  {
+    "revision": "bc5cdaa09cfbcaa742a3",
+    "url": "/portfolio_v3/static/js/runtime-main.dddd1dd0.js"
+  },
+  {
+    "revision": "e815187d074888fa589da14186b02bb3",
+    "url": "/portfolio_v3/static/media/bg.e815187d.jpg"
+  },
+  {
+    "revision": "4b934e824b40c51cf0842e7b29ee3191",
+    "url": "/portfolio_v3/static/media/covindia.4b934e82.PNG"
+  },
+  {
+    "revision": "044be97de40b56a56d80a6f037360dfa",
+    "url": "/portfolio_v3/static/media/poketwo.044be97d.PNG"
+  },
+  {
+    "revision": "8822fe8210883af8d9b4399e8db5f6c3",
+    "url": "/portfolio_v3/static/media/preact.8822fe82.png"
+  },
+  {
+    "revision": "f006806312dbf953a01945bc57c25740",
+    "url": "/portfolio_v3/static/media/pytorch.f0068063.png"
+  },
+  {
+    "revision": "3b220bca5005184285356d54547d4901",
+    "url": "/portfolio_v3/static/media/rust.3b220bca.png"
+  }
+]);
